492c451b7b82160f9e7128d70c3b3ef9cb21cc9b
Astalum <taaro20040526@icloud.com>
2025-05-23 08:41:27 +0000

    devcontainerに移植

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-    "net/http"
-    "github.com/gin-gonic/gin"
-)
+import "fmt"
 
 func main() {
-    r := gin.Default()
-    r.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{"message": "Hello, Gin on Windows!"})
-    })
-    r.Run(":8080") // 8080番ポートで起動
+    fmt.Println("Hello, World!")
 }
