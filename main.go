7a4c26820f0ee98ac173dfa36deded37511628f2
gyu5 <komanao700@gmail.com>
2025-06-11 15:10:11 +0900

    動かない

--- main.go
+++ main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
+
+type Todo struct {
+	ID		uint	`gorm:"primaryKey" json:"id"`
+	Title	string 	`json:"title"`
+	Completed	bool `json:"completed"`
+}
+
+var db *gorm.DB
+
+func initDB() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Todo{})
+}
+
+func createTodo(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Create(&todo)
+	c.JSON(http.StatusCreated, todo)
+}
+
+func getTodos(c *gin.Context) {
+	var todos []Todo
+	db.Find(&todos)
+	c.JSON(http.StatusOK, todos)
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, World!",
-		})
-	})
+
+	initDB()
+
+	r.POST("/todos", createTodo)
+
+	r.GET("/todos", getTodos)
+
 	r.Run(":8080")
 }
