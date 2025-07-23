8ac9857eaf1155a7e555ce6f6656c568ed7bfee4
Astalum <taaro20040526@icloud.com>
2025-07-23 15:23:57 +0900

    serviceの修正

--- services/expense_service.go
+++ services/expense_service.go
@@ -11,8 +11,9 @@ import (
 
 // インターフェースの定義
 type IExpenseService interface {
-	FindAllExpense(userId string) (*[]models.Expense, error)
+	GetExpenseCategorySummary(userId string) (*[]dto.GetExpenseCategorySummary, error)
 	CreateExpense(input dto.CreateExpenseInput) (*models.Expense, error)
+	DeleteExpense(userId string, expenseId string) error
 }
 
 // サービスの定義
@@ -26,8 +27,8 @@ func NewExpenseService(repository repositories.IExpenseRepository) IExpenseServi
 }
 
 // ユーザーごとの全ての支出を取得する関数の定義
-func (s *ExpenseService) FindAllExpense(userId string) (*[]models.Expense, error) {
-	return s.repository.FindAllExpense(userId)
+func (s *ExpenseService) GetExpenseSummary(userId string) (*[]dto.GetExpenseSummary, error) {
+	return s.repository.GetExpenseSummary(userId)
 }
 
 // 支出を作成する関数の定義
@@ -48,4 +49,14 @@ func (s *ExpenseService) CreateExpense(input dto.CreateExpenseInput) (*models.Ex
 	}
 
 	return s.repository.CreateExpense(newExpense)
+}
+
+// 支出を削除する関数の定義
+func (s *ExpenseService) DeleteExpense(userId string, expenseId string) error {
+	expenseUUID, err := uuid.Parse(expenseId)
+	if err != nil {
+		return err
+	}
+
+	return s.repository.DeleteExpense(userId, expenseUUID)
 }
